f4b3d73a25b15385f5017c7df6fb1648baf55526
odin <masharyahmed123@gmail.com>
2024-05-06 01:19:50 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class = "header" style = "display:flex">
+        <div></div>
+    </div>
+    <div class="body" style = "display:flex">
+        <div class = "left"></div>
+        <div class = "middle">
+            <div class = "recent" style = "display:flex; flex-direction:column">
+            </div>
+            <div class = "top" style = "display:flex; flex-direction:column"></div>
+        </div>
+        <div class = "right"></div>
+    </div>
+</body>
+</html>
