2bc8ccfd615770c45ee14947e107febcd8f7ab3e
Tsugair <145322188+TurkiAlsugair@users.noreply.github.com>
2024-05-07 23:25:29 +0300

    Update index.php
    This is the new Header

--- index.php
+++ index.php
@@ -8,28 +8,32 @@
 </head>
 <body>
  <!-- Header part-->  
-    <div class="header">
-        <div class = "logo"> <a href="https://stackoverflow.com">
-            <img src="images/stack.png" alt="logo"></a>  <b>StackOverFlow</b>
-        </div>
-        <div class = "SrchBar">
-            <form action="">
-                <input style= "padding-left:15px" type="text" name="searchBar" id="searchBar" placeholder="Search....">
-            </form>
-        </div>
-        <div class = "acc" ><svg style = "color: orange;" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
-            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
-            </svg>
-        </div>
-        <div class = "login"><a href ="https://stackoverflow.com" id = "login-hover" style = "width:10px; display: flex;
-            justify-content: center;
-            align-items: center; text-decoration: none" >Login</a>
-        </div>
-        <div class = "signUp"><a href ="https://stackoverflow.com" id = "login-hover" style = "width:10px; display: flex;
-            justify-content: center;
-            align-items: center;text-decoration: none;"  >Sign-Up</a>
-        </div>
-    </div>
+    
+<div class="header">
+<div class = "logo"> <a href="https://stackoverflow.com">
+<img src="logoSOV.png" alt="logo"></a>  <b>StackOverFlow</b></div>
+<div class = "SrchBar">
+  <form action="">
+    <input style= "padding-left:15px" type="text" name="searchBar" id="searchBar" placeholder="Search....">
+  </form>
+</div>
+<div class = "acc" ><svg style = "color: orange;" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+</svg></div>
+<div class = "login"><a href ="https://stackoverflow.com" id = "login-hover" style = "width:10px display: flex;
+    justify-content: center;
+    align-items: center; text-decoration: none" >Login</a></div>
+
+<div class = "signUp"><a href ="https://stackoverflow.com" id = "login-hover" style = "width:10px display: flex;
+    justify-content: center;
+    align-items: center;text-decoration: none;"  >Sign-Up</a></div>
+</div>
+
+<div class="content">
+  
+<div class="square">
+<div class="logo1"><a href="https://stackoverflow.com">
+<img src="logoSOV.png" alt="logo"></a></div>
 
 <!-- Body part-->
     <div class="body">
