526dae1182a5b5b9bd1167c5d9fd00343ff49e2e
odin <masharyahmed123@gmail.com>
2024-05-07 17:40:28 +0300

    finish header

--- index.php
+++ index.php
@@ -4,28 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel = "stylesheet" href = "styles/headerStyle.css">
 </head>
 <body>
     <div class="header">
-<div class = "logo"> <a href="https://stackoverflow.com">
-<img src="logoSOV.png" alt="logo"></a>  <b>StackOverFlow</b></div>
-<div class = "SrchBar">
-  <form action="">
-    <input type="text" name="searchBar" id="searchBar" placeholder="Search....">
-  </form>
-</div>
-<div class = "acc" >Account</div>
-<div class = "login">Login</div>
-<div class = "signUp">SignUP</div>
-</div>
-    <div class="body" style = "display:flex">
-        <div class = "left"></div>
-        <div class = "middle">
-            <div class = "recent" style = "display:flex; flex-direction:column">
-            </div>
-            <div class = "top" style = "display:flex; flex-direction:column"></div>
+        <div class = "logo"> <a href="https://stackoverflow.com">
+            <img src="images/image.png" alt="logo"></a>  <b>StackOverFlow</b>
         </div>
-        <div class = "right"></div>
+        <div class = "SrchBar">
+        <form action="">
+            <input type="text" name="searchBar" id="searchBar" placeholder="Search....">
+        </form>
+        </div>
+            <div class = "acc" >Account</div>
+            <div class = "login">Login</div>
+            <div class = "signUp">SignUP</div>
+        </div>
+    <div class="body">
+        <div class = "left-body-container">
+
+        </div>
+        <div class = "middle-body-container">
+                    
+        </div>
+        <div class = "right-body-container">
+
+        </div>
     </div>
 </body>
 </html>
