11efdc51cfa5db62bad41aa1f60415e25c8fa6c6
odin <masharyahmed123@gmail.com>
2024-05-11 23:12:22 +0300

    add answers info

--- scripts/pages.js
+++ scripts/pages.js
@@ -3,7 +3,7 @@ document.querySelectorAll(".QA-button").forEach(button => button.addEventListene
 function addQuestionsPage(nbOfQuestions, containerId)
 {
     let pagesContainer = document.querySelector("#" + containerId);
-    let buttonContainer = document.querySelector(".pages-button-container");
+    let buttonContainer = document.querySelector(".question-pages-button-container");
     nbOfPages = Math.ceil(nbOfQuestions/10);
 
     for(let i = 0; i < nbOfPages; i++)
@@ -14,16 +14,18 @@ function addQuestionsPage(nbOfQuestions, containerId)
         pagesContainer.appendChild(page);
         page.setAttribute("id","page" + i);
         if(nbOfQuestions >= 10)
-            addQuestions(page.getAttribute("id"),10);
+            addQuestionCards(page.getAttribute("id"),10);
         else
-            addQuestions(page.getAttribute("id"), nbOfQuestions);
+            addQuestionCards(page.getAttribute("id"), nbOfQuestions);
         page.style.display ="none";
         
         //create a button for each page
         let pageButton = document.querySelector(".pageButton").cloneNode(true);
         pageButton.setAttribute("id", "pageButton" + i);
         pageButton.innerHTML = i + 1;
-        pageButton.addEventListener("click",changeDisplayedPage);
+        pageButton.addEventListener("click",(e) => {
+            let target = e.target;
+            changeDisplayedPage(target,pagesContainer)});
         pageButton.style.display = "inline"
         buttonContainer.appendChild(pageButton);
 
@@ -31,26 +33,91 @@ function addQuestionsPage(nbOfQuestions, containerId)
         nbOfQuestions -= 10;
     }
     //display the first page as default
-    document.querySelector("#page0").style.display = "block";
+    pagesContainer.querySelector("#page0").style.display = "block";
+    //make question button background orange
+    document.querySelector("#question-type").classList.add("active");
+    //make page1 button background orange
+    buttonContainer.querySelector("#pageButton0").classList.add("active");
 }
 
-function addQuestions(containerId,nbOfQuestions)
+function addAnswersPage(nbOfAnswers, containerId)
 {
-    let questionsContainer = document.querySelector("#" + containerId);
-    let card = document.querySelector(".question")
+    let pagesContainer = document.querySelector("#" + containerId);
+    let buttonContainer = document.querySelector(".answer-pages-button-container");
 
-    for(let i = 0; i<nbOfQuestions; i++)
+    nbOfPages = Math.ceil(nbOfAnswers/10);
+    for(let i = 0; i < nbOfPages; i++)
+        {
+            //create page and add answers to it
+            let page = document.createElement("div");
+            //some functions use the question-container class to find the cards, any css specified for cards will be done using answer container to overwrite
+            page.classList.add("question-container");
+
+            page.classList.add("answer-container");
+            pagesContainer.appendChild(page);
+            page.setAttribute("id","page" + i);
+            if(nbOfAnswers >= 10)
+                addAnswerCards(pagesContainer, page.getAttribute("id"),10,);
+            else
+                addAnswerCards(pagesContainer, page.getAttribute("id"), nbOfAnswers);
+            page.style.display ="none";
+            
+            //create a button for each page
+            let pageButton = document.querySelector(".pageButton").cloneNode(true);
+            pageButton.setAttribute("id", "pageButton" + i);
+            pageButton.innerHTML = i + 1;
+            pageButton.addEventListener("click",(e) => {
+                let target = e.target;
+                changeDisplayedPage(target,pagesContainer)});
+            pageButton.style.display = "inline"
+            buttonContainer.appendChild(pageButton);
+    
+            //to use at the last page to know how many questions are left
+            nbOfAnswers -= 10;
+        }
+
+    //display the first page as default
+    pagesContainer.querySelector("#page0").style.display = "block";
+    //make question button background orange
+    document.querySelector("#question-type").classList.add("active");
+    //make page1 button background orange
+    buttonContainer.querySelector("#pageButton0").classList.add("active");
+}
+
+
+function addQuestionCards(containerId,nbOfCards)
+{
+    let container = document.querySelector("#" + containerId);
+    let card = document.querySelector(".questionCard")
+
+    for(let i = 0; i<nbOfCards; i++)
         {   
-            let actualCard = changeQuestionAtt(i, card);
+            let actualCard = changeQuestionCardAtt(i, card);
+
             //temp solution until I figure out what is changing the display to block
             actualCard.style.display = "flex";
-            questionsContainer.appendChild(actualCard);
+
+            container.appendChild(actualCard);
+        }
+}
+
+function addAnswerCards(pageContainer, pageId,nbOfCards)
+{
+    let container = pageContainer.querySelector("#" + pageId);
+    let card = document.querySelector(".answerCard");
+    for(let i = 0; i<nbOfCards; i++)
+        {   
+            let actualCard = changeAnswerCardAtt(i, card);
+            //temp solution until I figure out what is changing the display to block
+            actualCard.style.display = "flex";
+            container.appendChild(actualCard);
         }
 }
 
 
+
 //change the tag,answers,time, and question ids
-function changeQuestionAtt(questionNb,card)
+function changeQuestionCardAtt(questionNb,card)
 {
     let actualCard = card.cloneNode(true);
     actualCard.setAttribute("id", actualCard.getAttribute("id") + questionNb);
@@ -70,9 +137,23 @@ function changeQuestionAtt(questionNb,card)
     return actualCard;
 }
 
-function addInfoToPages()
+function changeAnswerCardAtt(answerNb,card)
 {
+    let actualCard = card.cloneNode(true);
+    actualCard.setAttribute("id", actualCard.getAttribute("id") + answerNb);
+    tagContainer = actualCard.querySelector("#qTag");
+    tagContainer.setAttribute("id", tagContainer.getAttribute("id") + answerNb);
 
+    timeContainer = actualCard.querySelector("#aTime");
+    timeContainer.setAttribute("id", timeContainer.getAttribute("id") + answerNb);
+
+    nbOfAnswersContainer = actualCard.querySelector("#aRating");
+    nbOfAnswersContainer.setAttribute("id", nbOfAnswersContainer.getAttribute("id") + answerNb);
+
+    textContainer = actualCard.querySelector("#answer");
+    textContainer.setAttribute("id",textContainer.getAttribute("id") + answerNb);
+
+    return actualCard;
 }
 
 function addQuestionsInfo(questions, tags, containerId)
@@ -112,32 +193,126 @@ function addQuestionsInfo(questions, tags, containerId)
     }
 } 
 
-function changeDisplayedPage(e)
+function addAnswersInfo(answers, avgRating, tags, containerId)
 {
-    let pageNumber = e.target.getAttribute("id").match(/\d+$/)[0];
-    let pageToDisplay = document.querySelector("#page" + pageNumber);
+    console.log(answers);
+    console.log(avgRating);
+    console.log(tags);
+    console.log(containerId);
+
+    nbOfPages = Math.ceil(answers.length /10);
+    let pagesContainer = document.querySelector("#" + containerId);
+    let nbOfAnswers = answers.length;
+    for(let i=0; i< nbOfPages; i++)
+    {  
+        let answersContainer = pagesContainer.querySelector("#page" + i);
+        let loopIterator;
+        if(nbOfAnswers >= 10)
+            loopIterator = 10;
+        else
+            loopIterator = nbOfAnswers;
+
+        for(let j=0; j<loopIterator; j++)
+        {
+            answersContainer.querySelector("#aRating" + j).innerHTML = avgRating[j + (i *10)];
+
+            //manual elipsis
+            let description;
+            if(answers[j + (i *10)]["description"].length > 40)
+                {
+                    description = answers[j + (i *10)]["description"].substring(0,40) + "...";
+                    console.log(description); 
+                }
+            else
+                description = answers[j + (i *10)]["description"];
+
+            answersContainer.querySelector("#answer" + j).innerHTML = description;
+            answersContainer.querySelector("#aTime" + j).innerHTML = "Answered by " + answers[j + (i *10)]["userName"] + " at " + answers[j + (i *10)].created_at; 
+
+            let tagContainer = answersContainer.querySelector("#qTag" + j);
+            for(let k = 0; k<tags[j].length ; k++)
+            {
+                let tag = document.createElement("span")
+                tag.classList.add("tag");
+                //add tag j for question i
+                tag.innerHTML = tags[j][k].tagName;
+                tagContainer.appendChild(tag);
+            }
+            answersContainer.querySelector("#card" + j).style.display = "flex";
+        }
+
+        //to use at the last page to know how many questions are left
+        nbOfAnswers -= 10;
+    }
+}
+
+
+function changeDisplayedPage(target,pagesContainer)
+{
+    let pageNumber = target.getAttribute("id").match(/\d+$/)[0];
+    let pageToDisplay = pagesContainer.querySelector("#page" + pageNumber);
     pageToDisplay.style.display = "block";
 
-    let allPages = document.querySelectorAll(".question-container");
+    //this is kinda hard coded needs changing
+    let allPages = pagesContainer.querySelectorAll(".question-container");
+
         allPages.forEach(page => {
             let computedStyle = window.getComputedStyle(page);
             if (computedStyle.getPropertyValue('display') === 'block' && page.getAttribute("id") != pageToDisplay.getAttribute("id"))
                 page.style.display = "none";
         });
+
+    //tmp hard coded solution to not mix answer and questions pages buttons
+    let buttonContainer;
+    if(pagesContainer.getAttribute("id") == "questions-pages-container")
+    {
+        buttonContainer = document.querySelector("#question-pages-button-container");
+    }
+    else 
+    {
+        buttonContainer = document.querySelector("#answer-pages-button-container");
+    }
+        
+    //add orange background to current page number
+    currentButton = buttonContainer.querySelector("#pageButton" + pageNumber);
+    currentButton.classList.add("active");
+
+    //remove the orange background from the previos page number 
+    let allPageButtons = buttonContainer.querySelectorAll(".pageButton");
+    allPageButtons.forEach(button => {
+            let classes = button.classList;
+            if (classes.contains("active") && button.getAttribute("id") != currentButton.getAttribute("id"))
+                button.classList.remove("active");
+        });
+    
 }
 
 
 function changeDisplayedPageType(e)
 {
-    let typeDisplayed = e.target;
-    if(typeDisplayed.getAttribute("id") == "question-type")
+    let buttonType = e.target;
+    if(buttonType.getAttribute("id") == "question-type")
     {
+        //display the question page and their buttons and add class active to the question button
         document.querySelector("#questions-pages-container").style.display = "block";
+        document.querySelector(".question-pages-button-container").style.display = "block";
+        buttonType.classList.add("active");
+        
+
         document.querySelector("#answers-pages-container").style.display = "none";
+        document.querySelector(".answer-pages-button-container").style.display = "none";
+        document.querySelector("#answer-type").classList.remove("active");
     }
     else 
     {
+        //display the answer page and add class active to the answer bautton
         document.querySelector("#questions-pages-container").style.display = "none";
+        document.querySelector(".question-pages-button-container").style.display = "none";
+        document.querySelector("#question-type").classList.remove("active");
+
         document.querySelector("#answers-pages-container").style.display = "block";
+        document.querySelector(".answer-pages-button-container").style.display = "block";
+        buttonType.classList.add("active");
     }
 }
+
--- userHome.php
+++ userHome.php
@@ -36,11 +36,20 @@ if ($conn->connect_error) {
         </div>
 
         <div class="middle-body-container">
-            <div>
-                <button type = "button" class ="QA-button" id = "question-type">Questions</button> <button type = "button" class ="QA-button" id = "answer-type">Answers</button>
+            <div class="user-info">
+                <img src = "images/user.png" alt="User Image">
+                <span>Username</span>                     
             </div>
+            <div class="bio">
+                <h3>bio</h3>
+                <p>This is where the user's bio information will be displayed.</p>
+            </div>
+                <div class="toggle-switch">
+                    <button type = "button" class ="QA-button" id = "question-type">Questions</button> 
+                    <button type = "button" class ="QA-button" id = "answer-type">Answers</button>
+                </div>
             <div class="container pages-container" id = "questions-pages-container">
-                <div class="question" id="card" style = "display: none;" > <!--container of one card-->
+                <div class="questionCard" id="card" style = "display: none;" > <!--container of one card-->
                         <div class="left-container">
                             <!--div that consist of votes div, answers div, and question-content-tag-->
                             <div class="answers">
@@ -57,25 +66,27 @@ if ($conn->connect_error) {
                         </div>
                     </div>
             </div>
-            <div class="container pages-container" id = "answers-pages-container" style = "display:none">
-                <div class="question" id="card" style = "display: none;" > <!--container of one card-->
-                        <div class="left-container">
-                            <!--div that consist of votes div, answers div, and question-content-tag-->
-                            <div class="answers">
-                                <p id="qAnswers">0</p> <!--number of answers here -->
-                                <p> answers </p>
-                            </div>
-                            <div class="question-content-tag">
-                                <div><a id="question" href="#">Question here </a></div>
-                                <p id="qTag"></p> <!--tags (each tag will have a span)-->
-                            </div>
+            <div class="container pages-container" id = "answers-pages-container" style = "display: none">
+                <div class="answerCard" id="card" style = "display: none;">
+                    <div class="left-container">
+                        <div class="answers">
+                            <p id="aRating">0</p> 
+                            <p> AVG Rating </p>
                         </div>
-                        <div class="time" id="qTime">
-                            asked 3 months ago by Abcd
+                        <div class="question-content-tag">
+                            <div class = "text"><a id="answer" href="#">Answer here </a></div>
+                            <p id="qTag"></p> <!--tags (each tag will have a span)-->
                         </div>
                     </div>
+                    <div class="time" id="aTime">
+                        answered 3 months ago by Abcd
+                    </div>
+                </div>
             </div>
-            <div class = "pages-button-container">
+            <div class = "question-pages-button-container" id = "question-pages-button-container">
+                <button type= "button" class = "pageButton" id ="pageButton" style = "display:none" >nb</button>
+            </div>
+            <div class = "answer-pages-button-container" id = "answer-pages-button-container" style = "display:none">
                 <button type= "button" class = "pageButton" id ="pageButton" style = "display:none" >nb</button>
             </div>
         </div>
@@ -89,9 +100,10 @@ if ($conn->connect_error) {
      $result = mysqli_query($conn,"SELECT q.question_id, q.username, q.title, q.description, q.created_at, COUNT(a.answer_id) AS num_answers
      FROM question q
      join answer a on q.question_id = a.question_id
-     WHERE q.username = 'alex_123'
      GROUP BY q.question_id, q.title, q.description
      ORDER BY q.created_at DESC");
+
+    echo "";
 
     $questions = array();
     while($row = mysqli_fetch_assoc($result))
@@ -113,4 +125,59 @@ if ($conn->connect_error) {
     echo "<script>";
     //addQuestions() is called here to ensure that addRecentQuestionsInfo() is only called after the cards are loaded
     echo "addQuestionsPage($nbOfQuestions, '$containerId'); addQuestionsInfo($questions, $tags, '$containerId');";    
-    echo "</script>"; ?> 
+    echo "</script>"; 
+?> 
+
+<?php 
+     $result = mysqli_query($conn,"SELECT *
+     FROM answer
+     ORDER BY created_at DESC");
+
+    $answers = array();
+    while($row = mysqli_fetch_assoc($result))
+        $answers[] = $row;
+
+    $rating = array();
+    for($i = 0; $i< count($answers); $i++)
+    {
+        $rating[$i] = array();
+        $curr = $answers[$i]["answer_id"];
+        $result = mysqli_query($conn,"SELECT rating FROM rating WHERE answer_id = $curr");
+        while($row = mysqli_fetch_assoc($result))
+            $rating[$i][] = $row;
+    }
+
+    $avgRating = array();
+    for($i = 0; $i< count($rating); $i++)
+    {
+        $sum = 0;
+        for($j = 0; $j<count($rating[$i]); $j++)
+            $sum += $rating[$i][$j]['rating'];
+        if(count($rating[$i]) != 0)
+            $avgRating[] = $sum / count($rating[$i]);
+        else
+            $avgRating[] = 0;
+
+    }
+
+    $tags = array();
+    for($i = 0; $i< count($answers); $i++)
+     {
+        $tags[$i] = array();
+        $curr = $answers[$i]["question_id"];
+        $result = mysqli_query($conn,"SELECT tagName FROM tag WHERE question_id = $curr");
+        while($row = mysqli_fetch_assoc($result))
+        $tags[$i][] = $row;
+     }
+
+    $nbOfAnswers = count($answers);
+    $answers  = json_encode($answers);
+    $avgRating = json_encode($avgRating);
+    $tags = json_encode($tags);
+    $containerId = "answers-pages-container";
+    echo "<script>";
+    //addQuestions() is called here to ensure that addRecentQuestionsInfo() is only called after the cards are loaded
+    echo "addAnswersPage($nbOfAnswers, '$containerId'); addAnswersInfo($answers, $avgRating , $tags, '$containerId');";     
+    echo "</script>"; 
+?> 
+
